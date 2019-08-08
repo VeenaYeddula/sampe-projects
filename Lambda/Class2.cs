632de8b4e67e17f0ca6439df8d54c6787b1b5d78
Lokesh <Lokesh@LAPTOP-I4EK9S4B>
2019-08-08 16:40:01 +0530

    Lambda samples

---
+++ Lambda/Class2.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace PROGRAMS
+{
+    public delegate void calculatorDelegate(int a, int b, int c);
+    public delegate string sayDelegate(string name);
+    public delegate void MathDelegate(int x, int y);
+}
+
+
---
+++ Lambda/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROGRAMS
+{
+    class DelDemo2
+    {
+        static void Main()
+        {
+            sayDelegate sd = name => "hello" + name + "have a nice day.";
+            calculatorDelegate cd = (a, b, c) => Console.WriteLine(a * b * c);
+            Console.WriteLine(sd("raju"));
+            Console.WriteLine(sd("naresh"));
+            Console.WriteLine(sd("nacre"));
+            cd(10, 20, 30); cd(40, 50, 60); cd(70, 80, 80);
+            Console.ReadLine();
+
+
+        }
+    }
+}
