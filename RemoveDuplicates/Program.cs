a434669f82ce5d0572e75384002e7b448df72f55
Lokesh <Lokesh@LAPTOP-I4EK9S4B>
2019-08-12 14:31:23 +0530

    added remove duplicates

---
+++ RemoveDuplicates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myStr = "kkllmnn";
+            Console.WriteLine("Initial String: " + myStr);
+            var unique = new HashSet<char>(myStr);
+            Console.Write("New String after removing duplicates: ");
+            foreach (char c in unique)
+                Console.Write(c);
+        }
+    }
+}
