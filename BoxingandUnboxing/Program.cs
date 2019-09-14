02a5e09b3195572853fa884689c26ea181c4a118
Lokesh <Lokesh@LAPTOP-I4EK9S4B>
2019-09-14 18:25:03 +0530

    Adding Boxing and unboxing sample

---
+++ BoxingandUnboxing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BoxingandUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boxing : value type to reference type
+            int a = 10;
+            object obj = a;
+            Console.WriteLine("value type to reference type is:{0}", a);
+            //UnBoxing : Reference type to Value type
+            int i= (int)obj;
+            Console.WriteLine("Reference type to value type is:{0}", i);
+
+        }
+    }
+}
