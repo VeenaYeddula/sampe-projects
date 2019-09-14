6c77d871c026282d48cd28487dd7804fdbea1183
Lokesh <Lokesh@LAPTOP-I4EK9S4B>
2019-09-14 18:39:08 +0530

    Adding inheritance sample

---
+++ Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class A
+    {
+       public void Method1()
+        {
+            Console.WriteLine("calling method1");
+        }
+        public void Method2()
+        {
+            Console.WriteLine("calling method2");
+        }
+    }
+  public class B:A
+    {
+        public void Method3()
+        {
+            Console.WriteLine("calling method3");
+        }
+        public void Method4()
+        {
+            Console.WriteLine("calling method4");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            B obj = new B();
+            obj.Method1();
+            obj.Method1();
+            obj.Method3();
+            obj.Method3();
+            obj.Method4();
+            Console.ReadLine();
+        }
+    }
+}
